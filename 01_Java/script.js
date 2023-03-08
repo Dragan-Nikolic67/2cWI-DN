4baa87624460cf47ce6cfbc56b8bed01e69c5aa8
Dragan-Nikolic67 <nesamskolovao2@gmail.com>
2023-03-08 08:58:37 +0100

    finished programming

---
+++ 01_Java/script.js
@@ -0,0 +1,74 @@
+/* //Video Aufgabe
+console.log("Hallo");
+console.log("Tolle Sache");
+
+//Variablen
+let a = 7;
+let b = 8;
+let c = a - b;
+
+console.log("A hat den Wert " + a);
+
+a = 10;
+console.log("A hat den Wert " + a);
+
+console.log("a + b = " + c);
+
+//Rechnungsarten
+// *, /, +, -
+
+c = b - a;
+console.log("b - a = " + c); */
+
+let a = 7;
+let b = 8;
+let c = 10;
+
+//Aufgabe a+b+c
+let result = a + b + c;
+console.log("Das Ergebnis ist " + result);
+
+//Aufgabe (a+b)/c
+result = ((a + b) / c)
+console.log("Das Ergebnis ist " + result);
+
+//Aufgabe c-a+b
+result = (c - a + b)
+console.log("Das Ergebnis ist " + result);
+
+//Aufgabe a+b+c*3
+result = (a + b + c * 3)
+console.log("Das Ergebnis ist " + result);
+
+//Aufgabe (a+b+c)/4
+result = ((a + b + c) / 4)
+console.log("Das Ergebnis ist " + result);
+
+//Variabeln ändern
+a = 3
+b = a + 3
+c = c - 3
+
+console.log("a + 3= " + b)
+console.log("c - 3= " + c)
+
+//Aufgabe mit neuen Variabeln
+//Aufgabe a+b+c
+result = a + b + c;
+console.log("Das Ergebnis ist " + result);
+
+//Aufgabe (a+b)/c
+result = ((a + b) / c)
+console.log("Das Ergebnis ist " + result);
+
+//Aufgabe c-a+b
+result = (c - a + b)
+console.log("Das Ergebnis ist " + result);
+
+//Aufgabe a+b+c*3
+result = (a + b + c * 3)
+console.log("Das Ergebnis ist " + result);
+
+//Aufgabe (a+b+c)/4
+result = ((a + b + c) / 4)
+console.log("Das Ergebnis ist " + result);
