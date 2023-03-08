1ab5c6ab741e0cb948b31296993eb3b5f14e248f
Dragan-Nikolic67 <nesamskolovao2@gmail.com>
2023-03-08 09:31:29 +0100

    finished

---
+++ script.js
@@ -0,0 +1,13 @@
+//Aufgabe 1
+let a = 17.4;
+let b = 3.9;
+
+let result = a / b;
+
+console.log("The result of " + a + " divided by " + b + " is " + result);
+
+//Aufgabe 2
+let Vorname = "Dragan";
+let Nachname = "Nikolic";
+
+console.log("Ich heiße " + Vorname + " " + Nachname);
