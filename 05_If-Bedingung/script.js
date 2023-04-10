c886f33285e4d54a2a4906480e8906ca4191e34d
Dragan-Nikolic67 <nesamskolovao2@gmail.com>
2023-04-10 16:34:23 +0200

    05_If-Bedingungen fertig

---
+++ 05_If-Bedingung/script.js
@@ -0,0 +1,23 @@
+let number = 60;
+
+if (number<20) {
+    console.log("Mini");
+}
+else if(number<50){
+    console.log("mediumm");
+}
+else  {
+    console.log("large");
+}
+
+number = 7;
+
+if (number<=7) {
+    console.log("Mini");
+}
+else if(number<90) {
+    console.log("Medium");
+}
+else {
+    console.log("Large");
+}
