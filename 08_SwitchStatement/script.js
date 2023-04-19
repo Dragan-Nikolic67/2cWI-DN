123ff024ea4479218b76b9d40d518c3bfca333ac
Dragan-Nikolic67 <nesamskolovao2@gmail.com>
2023-04-19 19:41:56 +0200

    08_SwitchStatement fertig

---
+++ 08_SwitchStatement/script.js
@@ -0,0 +1,23 @@
+let randomNumber = Math.random() * 5+6;
+let random = Math.floor(randomNumber);
+
+console.log(random);
+
+if ((random==10)) {
+    console.log("Ten");
+}
+else if (random==9){
+    console.log("Nine");
+}
+else if (random==8){
+    console.log("Eight");
+}
+else if (random==7){
+    console.log("Seven");
+}
+else if (random==6){
+    console.log("Six");
+}
+else if (random==5){
+    console.log("Five");
+}
